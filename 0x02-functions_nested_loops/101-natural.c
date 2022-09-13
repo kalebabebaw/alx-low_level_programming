5a414fe937f8ad1161646d071e75d13eb911bf37
kalebabebaw <emailtokaleb@gmail.com>
2022-09-13 13:58:29 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -20,8 +20,12 @@ int main(void)
 			d += b;
 			b += 3;
 		}
-		if  (c < e && (c % 3) != 0)
+
+		if (c < e && (c % 3) != 0)
 		{
+			d += c;
+			c += 5;
+		}
 		else if (c % 3 == 0)
 		{
 			c += 5;
