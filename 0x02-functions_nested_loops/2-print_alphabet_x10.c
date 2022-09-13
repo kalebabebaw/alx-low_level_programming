dc504b1da230ef976d294c62dd31cce001b0d4ad
kalebabebaw <emailtokaleb@gmail.com>
2022-09-13 07:06:34 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int x = 0
+		while (x < 10)
+		{
+			char y = 'a';
+			while (y <= 'z')
+			{
+				_putchar(y);
+				y++;
+			}
+			_putchar('\n');
+			x++;
+		}
+}
