1c127bd89c8443ba30f4aab34a3c0328db63a142
kalebabebaw <emailtokaleb@gmail.com>
2022-09-13 13:36:51 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 /**
- * main - print first 50 fibonacci
+ * main - first 50
  *
- * Retrun: 0 always.
+ * Return: always 0
  */
 int main(void)
 {
-	long int i, x = 1, y = 2, sum = 0;
-	for (i = 0: i < 49; i++)
+	int i = 0;
+	long j = 1;
+	long k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 50; i++)
 	{
-		printf("%ld, ", x);
-		sum = x + y;
-		x = y;
-		y = sum;
-		if (i == 48)
-			printf("%ld\n", x);
+		printf(", %lu", k);
+		k += j;
+		j = k - j;
 	}
+	printf("\n");
 	return (0);
 }
