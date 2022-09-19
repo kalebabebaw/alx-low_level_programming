3d63ae1e9edbb18e501e55ceebf29e6b7049ac7b
kalebabebaw <emailtokaleb@gmail.com>
2022-09-19 01:10:50 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,31 @@
 #include "main.h"
-/**
- * put_half - print the second half of the string
- */
-void puts_half(char *str)
-{
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+/**
+ * puts_half - prints every other character of a string
+ * @str: A pointer to an int that will be changed
+ *
+ * Return: void which means our answer is correct
+ */
+
+void puts_half(char *str)
+
+{
+	int i, last;
+
+	i = 0;
+	while (str[i] != '\0')
 	{
+		i++;
 	}
-	i++;
-	for (i = i / 2; str[i] != '\0'; i++)
+
+	last = (i + 1) / 2;
+
+	for (i = last; str[i]; i++)
 	{
-		_putchar(str[i];
-				}
-				_putchar('\n');
-				}
+		_putchar (str[i]);
+	}
+
+
+	_putchar ('\n');
+
+}
