56f5a69bedd75ec725ede37c39b2d32efde6174e
kalebabebaw <emailtokaleb@gmail.com>
2022-09-22 12:24:33 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,25 @@
 #include "main.h"
-/**
- * rot13 - econdes a string using rot 13
- * @s: input string.
- * Return: the pointer to dest.
- */
 
-char *rot13(char *s)
+/**
+ * rot13 - string using rot13
+ * @str: string to be encoded
+ *
+ * Return: to the string
+ */
+char *rot13(char *str)
 {
 	int count = 0, i;
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (*(s + count) != '\0')
+	while (*(str + count) != '\0')
 	{
 
 		for (i = 0; i < 52; i++)
 		{
-			if (*(s + count) == alphabet[i])
+			if (*(str + count) == alphabet[i])
 			{
-				*(s + count) == rot13[i];
+				*(str + count) = rot13[i];
 				break;
 			}
 		}
