d2a1714461548f206fe8ed7c529874a7391be0ab
kalebabebaw <emailtokaleb@gmail.com>
2022-09-30 04:05:41 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	(void) argc;
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
