227628ec4b9ec2e76a8979f2fd5bd436f752cd0c
kalebabebaw <emailtokaleb@gmail.com>
2022-10-06 05:35:34 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,6 +10,7 @@ int main(int argc, *argv[])
 {
 	unsigned long mul;
 	int i, j;
+
 	if (argc != 3)
 	{ printf("Error\n");
 		exit(98); }
@@ -23,7 +24,7 @@ int main(int argc, *argv[])
 		}
 
 	}
-	mul = atol(argv[1] * atol(argv[2]);
+	mul = atol(argv[1]) * atol(argv[2]);
 			printf("%lu\n", mul);
 			return (0);
 			}
