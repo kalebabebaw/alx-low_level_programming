59d4d5810e1e52b6952c1264dc2d8f392c49ef2a
kalebabebaw <emailtokaleb@gmail.com>
2022-12-15 00:51:58 -0800

    task o

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: the string to append
+ * @src: the string to appended to dest
+ * @n: int value
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
